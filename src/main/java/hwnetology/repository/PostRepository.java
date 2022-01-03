230eefe1631235ab7eedd050f894d98c55d5ac36
ILYA EGOROV <egorovcf@gmail.com>
2022-01-03 08:21:43 +0500

    Added chm/feature

--- src/main/java/hwnetology/repository/PostRepository.java
+++ src/main/java/hwnetology/repository/PostRepository.java
@@ -6,34 +6,34 @@ import org.springframework.stereotype.Repository;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Хранилище наших данных
 @Repository
 public class PostRepository {
 
-
-
-    private List<Post> postList;
+    private Map<Long, Post> postList;
     private static AtomicLong idGenerator;
     private final long DELTA_ID = 1;
 
     public PostRepository() {
-        postList = new CopyOnWriteArrayList<>();
+        postList = new ConcurrentHashMap<>();
         idGenerator = new AtomicLong(0);
 
         // Значения для проверки
-        postList.add(new Post(10, "First post"));
-        postList.add(new Post(15, "Second post"));
-        postList.add(new Post(20, "Weird post))))"));
+        postList.put(10L, new Post(10, "First post"));
+        postList.put(15L, new Post(15, "Second post"));
+        postList.put(20L, new Post(20, "Weird post))))"));
+        postList.get(20L).markAsRemoved();
     }
 
-    public synchronized List<Post> all() {
+    public List<Post> all() {
         // Создаём новый лист, который будет содержать только не отмеченные на удаление посты
         List<Post> actualList = new LinkedList<>();
-        for (Post currentPost : postList) {
+        for (Post currentPost : postList.values()) {
             if (currentPost.getRemovedFlag() == false) {
                 actualList.add(currentPost);
             }
@@ -41,8 +41,8 @@ public class PostRepository {
         return actualList;
     }
 
-    public synchronized Optional<Post> getById(long id) {
-        for (Post currentPost : postList) {
+    public Optional<Post> getById(long id) {
+        for (Post currentPost : postList.values()) {
             if (currentPost.getId() == id) {
                 return Optional.of(currentPost);
             }
@@ -50,18 +50,18 @@ public class PostRepository {
         return Optional.empty();
     }
 
-    public synchronized Optional<Post> save(Post post) {
+    public Optional<Post> save(Post post) {
         // если id = 0, то создаём новый пост
         Post freshPost = new Post();
         if (post.getId() == 0) {
             freshPost = new Post(idGenerator.addAndGet(DELTA_ID), post.getContent());
-            postList.add(freshPost);
+            postList.put(idGenerator.get(), freshPost);
         }
 
         // если id !=0, то изменяем имеющийся пост
         if (post.getRemovedFlag() == false) {
             if (post.getId() != 0) {
-                for (Post currentPost : postList) {
+                for (Post currentPost : postList.values()) {
                     if (currentPost.getId() == post.getId()) {
                         currentPost.setContent(post.getContent());
                         break;
@@ -78,8 +78,8 @@ public class PostRepository {
         return Optional.of(freshPost);
     }
 
-    public synchronized void removeById(long id) {
-        for (Post currentPost : postList) {
+    public void removeById(long id) {
+        for (Post currentPost : postList.values()) {
             if (currentPost.getId() == id) {
                 currentPost.markAsRemoved();
             }
