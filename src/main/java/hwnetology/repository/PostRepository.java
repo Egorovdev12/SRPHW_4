cce58fcfee1a28c326c6ea879117c523ee405145
ILYA EGOROV <egorovcf@gmail.com>
2022-01-03 08:12:48 +0500

    Added chm/master

--- src/main/java/hwnetology/repository/PostRepository.java
+++ src/main/java/hwnetology/repository/PostRepository.java
@@ -5,31 +5,32 @@ import hwnetology.model.Post;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Хранилище наших данных
 @Repository
 public class PostRepository {
 
-    private List<Post> postList;
+    private Map<Long, Post> postList;
     private static AtomicLong idGenerator;
     private final long DELTA_ID = 1;
 
     public PostRepository() {
-        postList = new CopyOnWriteArrayList<>();
+        postList = new ConcurrentHashMap<>();
         idGenerator = new AtomicLong(0);
     }
 
-    public synchronized List<Post> all() {
-        return postList;
+    public List<Post> all() {
+        return postList.values().stream().toList();
     }
 
-    public synchronized Optional<Post> getById(long id) {
+    public Optional<Post> getById(long id) {
         Post resultPost = new Post();
 
-        for (Post currentPost : postList) {
+        for (Post currentPost : postList.values()) {
             if (currentPost.getId() == id) {
                 resultPost = currentPost;
                 break;
@@ -38,17 +39,17 @@ public class PostRepository {
         return Optional.of(resultPost);
     }
 
-    public synchronized Post save(Post post) {
+    public Post save(Post post) {
         // если id = 0, то создаём новый пост
         Post freshPost = new Post();
         if (post.getId() == 0) {
             freshPost = new Post(idGenerator.addAndGet(DELTA_ID), post.getContent());
-            postList.add(freshPost);
+            postList.put(idGenerator.get(), freshPost);
         }
 
         // если id !=0, то изменяем имеющийся пост
         if (post.getId() != 0) {
-            for (Post currentPost : postList) {
+            for (Post currentPost : postList.values()) {
                 if (currentPost.getId() == post.getId()) {
                     currentPost.setContent(post.getContent());
                     break;
@@ -62,8 +63,8 @@ public class PostRepository {
         return freshPost;
     }
 
-    public synchronized void removeById(long id) {
-        for (Post currentPost : postList) {
+    public void removeById(long id) {
+        for (Post currentPost : postList.values()) {
             if(currentPost.getId() == id) {
                 postList.remove(currentPost);
             }
